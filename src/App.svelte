bce0eb7c2400bc3a3e0c977aa3534416caa7ce16
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2019-10-26 08:50:21 -0300

    Single commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Greeting from "./Greeting.svelte";
+
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+
+  h2 {
+    color: purple;
+    font-family: "Comic Sans MS";
+    font-size: 2em;
+  }
+</style>
+
+<Greeting firstName="Svelte" lastName="Dev" />
---
+++ src/Greeting.svelte
@@ -0,0 +1,63 @@
+<script>
+  export let firstName;
+  export let lastName;
+
+  $: name = firstName + " " + lastName;
+
+  let user = { loggedIn: false };
+  function toggleLogin() {
+    user.loggedIn = !user.loggedIn;
+  }
+
+  let frameworks = [
+    { id: 1, name: "Svelte" },
+    { id: 2, name: "Angular" },
+    { id: 3, name: "React" },
+    { id: 4, name: "Vue" }
+  ];
+
+  let m = { x: 0, y: 0 };
+  function handleMousemove(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+</script>
+
+<style>
+  h2 {
+    color: purple;
+    font-family: "Comic Sans MS";
+    font-size: 2em;
+  }
+  li {
+    list-style-type: none;
+    text-indent: 0;
+  }
+  div {
+    width: 300px;
+    height: 300px;
+    color: purple;
+    background: lightseagreen;
+  }
+</style>
+
+<label for="name">Nome:</label>
+<input bind:value={name} type="text" id="name">
+<h2>Hello {name}</h2>
+
+{#if user.loggedIn}
+  <span>User is logged!</span>
+  <button on:click={toggleLogin}>Logout</button>
+{:else}
+  <span>No user logged!</span>
+  <button on:click={toggleLogin}>Login</button>
+{/if}
+
+<h2>List of frameworks</h2>
+<ul>
+  {#each frameworks as { id, name }}
+    <li>{id}: {name}</li>
+  {/each}
+</ul>
+
+<div on:mousemove={handleMousemove}>The mouse position is {m.x} x {m.y}</div>
